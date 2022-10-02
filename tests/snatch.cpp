d1472a03e6c76a71cdaa5f0b36e2b05881be4500
Corentin Schreiber <github@corentin.net>
2022-10-02 09:52:42 +0100

    Added test filtering, verbose mode, clean up

--- tests/snatch.cpp
+++ tests/snatch.cpp
@@ -5,8 +5,127 @@
 #include <cstring>
 #include <set>
 
-namespace testing {
-namespace color {
+namespace {
+using namespace testing;
+using namespace testing::impl;
+
+template<typename F>
+bool run_tests(registry& r, F&& predicate) {
+    bool        success         = true;
+    std::size_t run_count       = 0;
+    std::size_t fail_count      = 0;
+    std::size_t assertion_count = 0;
+
+    for (std::size_t i = 0; i < r.test_count; ++i) {
+        test_case& t = r.test_list[i];
+        if (!predicate(t)) {
+            continue;
+        }
+
+        r.run(t);
+
+        ++run_count;
+        assertion_count += t.tests;
+        if (t.state == test_state::failed) {
+            ++fail_count;
+            success = false;
+        }
+    }
+
+    printf("==========================================\n");
+    if (success) {
+        printf(
+            "%ssuccess:%s all tests passed (%ld test cases, %ld assertions)\n", color::pass_start,
+            color::reset, run_count, assertion_count);
+    } else {
+        printf(
+            "%serror:%s some tests failed (%ld out of %ld test cases, %ld assertions)\n",
+            color::fail_start, color::reset, fail_count, run_count, assertion_count);
+    }
+
+    return success;
+}
+
+template<typename F>
+void list_tests(const registry& r, F&& predicate) {
+    for (std::size_t i = 0; i < r.test_count; ++i) {
+        const auto& t = r.test_list[i];
+        if (!predicate(t)) {
+            continue;
+        }
+
+        if (!t.type.empty()) {
+            printf(
+                "%.*s [%.*s]\n", static_cast<int>(t.name.length()), t.name.data(),
+                static_cast<int>(t.type.length()), t.type.data());
+        } else {
+            printf("%.*s\n", static_cast<int>(t.name.length()), t.name.data());
+        }
+    }
+}
+
+template<typename F>
+void for_each_tag(std::string_view s, F&& callback) {
+    std::string_view delim    = ",";
+    std::size_t      pos      = s.find(delim);
+    std::size_t      last_pos = 0u;
+
+    while (pos != std::string_view::npos) {
+        std::size_t cur_size = pos - last_pos;
+        if (cur_size != 0) {
+            callback(s.substr(last_pos, cur_size));
+        }
+        last_pos = pos + delim.size();
+        pos      = s.find(delim, last_pos);
+    }
+
+    callback(s.substr(last_pos));
+}
+
+std::size_t get_full_name_length(const test_case& t) {
+    // +3 for " []" surrounding the type
+    return t.name.length() + t.type.length() + 3;
+}
+
+std::string_view
+make_full_name(std::array<char, max_test_name_length>& buffer, const test_case& t) {
+    strncpy(buffer.data(), t.name.data(), t.name.length());
+    strncpy(buffer.data() + t.name.length(), " [", 2);
+    strncpy(buffer.data() + t.name.length() + 2, t.type.data(), t.type.length());
+    strncpy(buffer.data() + t.name.length() + t.type.length() + 2, "]", 1);
+    return std::string_view(buffer.data(), get_full_name_length(t));
+}
+
+template<typename T>
+constexpr const char* get_format_code() {
+    if constexpr (std::is_same_v<T, const void*>) {
+        return "%p";
+    } else if constexpr (std::is_same_v<T, std::size_t>) {
+        return "%zu";
+    } else if constexpr (std::is_same_v<T, std::ptrdiff_t>) {
+        return "%td";
+    } else {
+        static_assert(std::is_same_v<T, T>, "unsupported type");
+    }
+}
+
+template<typename T>
+void append_impl(expression& exp, T value) {
+    std::size_t length =
+        snprintf(exp.data.data() + exp.data_length, 0, get_format_code<T>(), value);
+
+    if (length > exp.available()) {
+        exp.failed = true;
+        return;
+    }
+
+    snprintf(exp.data.data() + exp.data_length, exp.available(), get_format_code<T>(), value);
+
+    exp.data_length += length;
+}
+} // namespace
+
+namespace testing::impl::color {
 const char* error_start      = "\e[1;31m";
 const char* warning_start    = "\e[1;33m";
 const char* status_start     = "\e[1;36m";
@@ -16,8 +135,9 @@ const char* pass_start       = "\e[1;32m";
 const char* highlight1_start = "\e[1;35m";
 const char* highlight2_start = "\e[1;36m";
 const char* reset            = "\e[0m";
-} // namespace color
+} // namespace testing::impl::color
 
+namespace testing::impl {
 std::string_view expression::str() const {
     return std::string_view(data.data(), data_length);
 }
@@ -46,15 +166,7 @@ void expression::append_str(const char* str) {
 }
 
 void expression::append_impl(const void* ptr) {
-    std::size_t length = snprintf(data.data() + data_length, 0, "%p", ptr);
-    if (length > available()) {
-        failed = true;
-        return;
-    }
-
-    snprintf(data.data() + data_length, available(), "%p", ptr);
-
-    data_length += length;
+    ::append_impl(*this, ptr);
 }
 
 void expression::append_impl(std::nullptr_t) {
@@ -62,27 +174,11 @@ void expression::append_impl(std::nullptr_t) {
 }
 
 void expression::append_impl(std::size_t i) {
-    std::size_t length = snprintf(data.data() + data_length, 0, "%zu", i);
-    if (length > available()) {
-        failed = true;
-        return;
-    }
-
-    snprintf(data.data() + data_length, available(), "%zu", i);
-
-    data_length += length;
+    ::append_impl(*this, i);
 }
 
 void expression::append_impl(std::ptrdiff_t i) {
-    std::size_t length = snprintf(data.data() + data_length, 0, "%td", i);
-    if (length > available()) {
-        failed = true;
-        return;
-    }
-
-    snprintf(data.data() + data_length, available(), "%td", i);
-
-    data_length += length;
+    ::append_impl(*this, i);
 }
 
 void expression::append_impl(bool value) {
@@ -100,19 +196,30 @@ void expression::append_impl(std::string_view value) {
 void expression::append_impl(const char* value) {
     append_str(value);
 }
+} // namespace testing::impl
 
+namespace testing {
 void registry::register_test(
     std::string_view name, std::string_view tags, std::string_view type, test_ptr func) {
 
-    if (test_count == max_tests) {
+    if (test_count == max_test_cases) {
         printf(
-            "%serror:%s max number of tests reached; "
-            "please increase 'max_tests' (currently %ld)\n.",
-            color::error_start, color::reset, max_tests);
+            "%serror:%s max number of test cases reached; "
+            "please increase 'max_test_cases' (currently %ld)\n.",
+            color::error_start, color::reset, max_test_cases);
         std::terminate();
     }
 
     test_list[test_count] = test_case{name, tags, type, func};
+
+    if (get_full_name_length(test_list[test_count]) > max_test_name_length) {
+        printf(
+            "%serror:%s max length of test name reached; "
+            "please increase 'max_test_name_length' (currently %ld)\n.",
+            color::error_start, color::reset, max_test_name_length);
+        std::terminate();
+    }
+
     ++test_count;
 }
 
@@ -154,9 +261,15 @@ void registry::print_details_expr(
 
 void registry::run(test_case& t) {
     if (verbose) {
-        printf(
-            "%sstarting:%s %s%.*s%s.\n", color::status_start, color::reset, color::highlight1_start,
-            static_cast<int>(t.name.length()), t.name.data(), color::reset);
+        printf("%sstarting:%s %s", color::status_start, color::reset, color::highlight1_start);
+        if (!t.type.empty()) {
+            printf(
+                "%.*s [%.*s]", static_cast<int>(t.name.length()), t.name.data(),
+                static_cast<int>(t.type.length()), t.type.data());
+        } else {
+            printf("%.*s", static_cast<int>(t.name.length()), t.name.data());
+        }
+        printf("%s.\n", color::reset);
     }
 
     t.tests = 0;
@@ -171,9 +284,15 @@ void registry::run(test_case& t) {
     }
 
     if (verbose) {
-        printf(
-            "%sfinished:%s %s%.*s%s.\n", color::status_start, color::reset, color::highlight1_start,
-            static_cast<int>(t.name.length()), t.name.data(), color::reset);
+        printf("%sfinished:%s %s", color::status_start, color::reset, color::highlight1_start);
+        if (!t.type.empty()) {
+            printf(
+                "%.*s [%.*s]", static_cast<int>(t.name.length()), t.name.data(),
+                static_cast<int>(t.type.length()), t.type.data());
+        } else {
+            printf("%.*s", static_cast<int>(t.name.length()), t.name.data());
+        }
+        printf("%s.\n", color::reset);
     }
 }
 
@@ -184,86 +303,38 @@ void registry::set_state(test_case& t, test_state s) {
     }
 }
 
-bool registry::run_all() {
-    bool        success         = true;
-    std::size_t run_count       = 0;
-    std::size_t fail_count      = 0;
-    std::size_t assertion_count = 0;
-    for (std::size_t i = 0; i < test_count; ++i) {
-        run(test_list[i]);
-        ++run_count;
-        assertion_count += test_list[i].tests;
-        if (test_list[i].state == test_state::failed) {
-            ++fail_count;
-            success = false;
-        }
-    }
-
-    printf("==========================================\n");
-    if (success) {
-        printf(
-            "%ssuccess:%s all tests passed (%ld cases, %ld assertions)\n", color::pass_start,
-            color::reset, run_count, assertion_count);
-    } else {
-        printf(
-            "%serror:%s some tests failed (%ld out of %ld cases, %ld assertions)\n",
-            color::fail_start, color::reset, fail_count, run_count, assertion_count);
-    }
-
-    return success;
+bool registry::run_all_tests() {
+    return run_tests(*this, [](const test_case&) { return true; });
 }
 
-namespace {
-template<typename F>
-void list_tests(const registry& r, F&& predicate) {
-    for (std::size_t i = 0; i < r.test_count; ++i) {
-        const auto& t = r.test_list[i];
-        if (!predicate(t)) {
-            continue;
-        }
+bool registry::run_tests_matching_name(std::string_view name) {
+    std::array<char, max_test_name_length> buffer;
+    return run_tests(*this, [&](const test_case& t) {
+        std::string_view v = make_full_name(buffer, t);
 
-        if (!t.type.empty()) {
-            printf(
-                "%.*s [%.*s]\n", static_cast<int>(t.name.length()), t.name.data(),
-                static_cast<int>(t.type.length()), t.type.data());
-        } else {
-            printf("%.*s\n", static_cast<int>(t.name.length()), t.name.data());
-        }
-    }
+        // TODO: use regex here?
+        return v.find(name) != t.name.npos;
+    });
 }
 
-std::vector<std::string_view> split_tags(std::string_view s) {
-    std::vector<std::string_view> tags;
-
-    std::string_view delim    = ",";
-    std::size_t      pos      = s.find(delim);
-    std::size_t      last_pos = 0u;
-    std::size_t      cur_size = 0u;
-
-    while (pos != std::string_view::npos) {
-        cur_size = pos - last_pos;
-        if (cur_size != 0) {
-            tags.push_back(s.substr(last_pos, cur_size));
-        }
-        last_pos = pos + delim.size();
-        pos      = s.find(delim, last_pos);
-    }
-
-    tags.push_back(s.substr(last_pos));
-
-    return tags;
+bool registry::run_tests_with_tag(std::string_view tag) {
+    return run_tests(*this, [&](const test_case& t) {
+        bool selected = false;
+        for_each_tag(t.tags, [&](std::string_view v) {
+            if (v == tag) {
+                selected = true;
+            }
+        });
+        return selected;
+    });
 }
-
-} // namespace
 
 void registry::list_all_tags() const {
     std::set<std::string_view> tags;
     for (std::size_t i = 0; i < test_count; ++i) {
         const auto& t = test_list[i];
 
-        for (auto sv : split_tags(t.tags)) {
-            tags.insert(sv);
-        }
+        for_each_tag(t.tags, [&](std::string_view v) { tags.insert(v); });
     }
 
     for (auto c : tags) {
@@ -277,12 +348,13 @@ void registry::list_all_tests() const {
 
 void registry::list_tests_with_tag(std::string_view tag) const {
     list_tests(*this, [&](const test_case& t) {
-        for (auto sv : split_tags(t.tags)) {
-            if (sv == tag) {
-                return true;
+        bool selected = false;
+        for_each_tag(t.tags, [&](std::string_view v) {
+            if (v == tag) {
+                selected = true;
             }
-        }
-        return false;
+        });
+        return selected;
     });
 }
 
@@ -294,11 +366,16 @@ int main(int argc, char* argv[]) {
 
     // clang-format off
     options.add_options()
+        ("tests", "A regex to select which test cases to run", cxxopts::value<std::string>())
         ("l,list-tests", "List tests by name")
-        ("list-tests-with-tag", "List tests by name with a given tag", cxxopts::value<std::string>())
         ("list-tags", "List tags by name")
+        ("list-tests-with-tag", "List tests by name with a given tag", cxxopts::value<std::string>())
+        ("t,tags", "Use tags for filtering, not name")
+        ("v,verbose", "Enable detailed output")
         ("h,help", "Print help");
     // clang-format on
+
+    options.parse_positional({"tests"});
 
     auto result = options.parse(argc, argv);
 
@@ -322,9 +399,17 @@ int main(int argc, char* argv[]) {
         return 0;
     }
 
-    return testing::tests.run_all();
+    if (result.count("verbose") > 0) {
+        testing::tests.verbose = true;
+    }
 
-    // } else {
-    //     return testing::tests.run_selected(argc - 1);
-    // }
+    if (result.count("tests") > 0) {
+        if (result.count("tags") > 0) {
+            return testing::tests.run_tests_with_tag(result["tests"].as<std::string>());
+        } else {
+            return testing::tests.run_tests_matching_name(result["tests"].as<std::string>());
+        }
+    } else {
+        return testing::tests.run_all_tests();
+    }
 }
--- tests/snatch.hpp
+++ tests/snatch.hpp
@@ -7,7 +7,38 @@
 #include <string_view>
 #include <tuple>
 
+#if !defined(SNATCH_MAX_TEST_CASES)
+#    define SNATCH_MAX_TEST_CASES 5'000
+#endif
+#if !defined(SNATCH_MAX_EXPR_LENGTH)
+#    define SNATCH_MAX_EXPR_LENGTH 1'024
+#endif
+#if !defined(SNATCH_MAX_TEST_NAME_LENGTH)
+#    define SNATCH_MAX_TEST_NAME_LENGTH 1'024
+#endif
+
 namespace testing {
+
+// Testing framework configuration.
+// --------------------------------
+// Maximum number of test cases in the whole program.
+// A "test case" is created for each uses of the `*_TEST_CASE` macros,
+// and for each type for the `TEMPLATE_LIST_TEST_CASE` macro.
+constexpr std::size_t max_test_cases = SNATCH_MAX_TEST_CASES;
+// Maximum length of a `CHECK(...)` or `REQUIRE(...)` expression,
+// beyond which automatic variable printing is disabled.
+constexpr std::size_t max_expr_length = SNATCH_MAX_EXPR_LENGTH;
+// Maximum length of a full test case name.
+// The full test case name includes the base name, plus any type.
+constexpr std::size_t max_test_name_length = SNATCH_MAX_TEST_NAME_LENGTH;
+
+// Forward declarations.
+// ---------------------
+struct registry;
+
+// Implementation details.
+// -----------------------
+namespace impl {
 template<typename T>
 constexpr std::string_view get_type_name() {
 #if defined(__clang__)
@@ -32,8 +63,6 @@ constexpr std::string_view get_type_name() {
 
     return function.substr(start, size);
 }
-
-struct registry;
 
 template<typename T>
 struct proxy;
@@ -68,8 +97,6 @@ struct test_case {
     std::size_t      tests = 0;
 };
 
-constexpr std::size_t max_tests = 5'000;
-
 namespace color {
 extern const char* error_start;
 extern const char* warning_start;
@@ -81,8 +108,6 @@ extern const char* highlight1_start;
 extern const char* highlight2_start;
 extern const char* reset;
 } // namespace color
-
-constexpr std::size_t max_expr_length = 1'024;
 
 struct expression {
     std::array<char, max_expr_length> data;
@@ -145,37 +170,46 @@ struct expression {
 
 #undef EXPR_OPERATOR
 };
+} // namespace impl
+
+// Test registry.
+// --------------
 
 struct registry {
-    std::array<test_case, max_tests> test_list;
-    std::size_t                      test_count = 0;
+    std::array<impl::test_case, max_test_cases> test_list;
+    std::size_t                                 test_count = 0;
 
     bool verbose = false;
 
     template<typename T>
-    proxy<T> add(std::string_view name, std::string_view tags) {
+    impl::proxy<T> add(std::string_view name, std::string_view tags) {
         return {this, name, tags};
     }
 
     void register_test(
-        std::string_view name, std::string_view tags, std::string_view type, test_ptr func);
+        std::string_view name, std::string_view tags, std::string_view type, impl::test_ptr func);
 
     template<typename... Args, typename F>
     void register_tests(std::string_view name, std::string_view tags, const F& func) {
-        (register_test(name, tags, get_type_name<Args>(), to_test_case_ptr<Args>(func)), ...);
+        (register_test(name, tags, impl::get_type_name<Args>(), impl::to_test_case_ptr<Args>(func)),
+         ...);
     }
 
-    void print_location(const test_case& current_case, const char* filename, int line_number) const;
+    void print_location(
+        const impl::test_case& current_case, const char* filename, int line_number) const;
 
     void print_failure() const;
     void print_skip() const;
     void print_details(const char* message) const;
-    void print_details_expr(const char* check, const char* exp_str, const expression& exp) const;
+    void
+    print_details_expr(const char* check, const char* exp_str, const impl::expression& exp) const;
 
-    void run(test_case& t);
-    void set_state(test_case& t, test_state s);
+    void run(impl::test_case& t);
+    void set_state(impl::test_case& t, impl::test_state s);
 
-    bool run_all();
+    bool run_all_tests();
+    bool run_tests_matching_name(std::string_view name);
+    bool run_tests_with_tag(std::string_view tag);
 
     void list_all_tests() const;
     void list_all_tags() const;
@@ -183,23 +217,31 @@ struct registry {
 };
 
 extern registry tests;
+} // namespace testing
 
+// Implementation details.
+// -----------------------
+
+namespace testing::impl {
 template<typename... Args>
 template<typename F>
 const char* proxy<std::tuple<Args...>>::operator=(const F& func) {
     tests->template register_tests<Args...>(name, tags, func);
     return name.data();
 }
-} // namespace testing
+} // namespace testing::impl
+
+// Test macros.
+// ------------
 
 #define TESTING_CONCAT_IMPL(x, y) x##y
 #define TESTING_MACRO_CONCAT(x, y) TESTING_CONCAT_IMPL(x, y)
-#define TESTING_EXPR(x) testing::expression{} <= x
+#define TESTING_EXPR(x) testing::impl::expression{} <= x
 
 #define TEMPLATE_LIST_TEST_CASE(NAME, TAGS, TYPES)                                                 \
     static const char* TESTING_MACRO_CONCAT(test_id_, __COUNTER__) =                               \
-        testing::tests.add<TYPES>(NAME, TAGS) =                                                    \
-            []<typename TestType>(testing::test_case & CURRENT_CASE [[maybe_unused]]) -> void
+        testing::tests.add<TYPES>(NAME, TAGS) = []<typename TestType>(                             \
+            testing::impl::test_case & CURRENT_CASE [[maybe_unused]]) -> void
 
 #define REQUIRE(EXP)                                                                               \
     do {                                                                                           \
@@ -209,7 +251,7 @@ const char* proxy<std::tuple<Args...>>::operator=(const F& func) {
             testing::tests.print_failure();                                                        \
             testing::tests.print_location(CURRENT_CASE, __FILE__, __LINE__);                       \
             testing::tests.print_details_expr("REQUIRE", #EXP, EXP2);                              \
-            throw testing::test_state::failed;                                                     \
+            throw testing::impl::test_state::failed;                                               \
         }                                                                                          \
     } while (0)
 
@@ -221,7 +263,7 @@ const char* proxy<std::tuple<Args...>>::operator=(const F& func) {
             testing::tests.print_failure();                                                        \
             testing::tests.print_location(CURRENT_CASE, __FILE__, __LINE__);                       \
             testing::tests.print_details_expr("CHECK", #EXP, EXP2);                                \
-            testing::tests.set_state(CURRENT_CASE, testing::test_state::failed);                   \
+            testing::tests.set_state(CURRENT_CASE, testing::impl::test_state::failed);             \
         }                                                                                          \
     } while (0)
 
@@ -230,7 +272,7 @@ const char* proxy<std::tuple<Args...>>::operator=(const F& func) {
         testing::tests.print_failure();                                                            \
         testing::tests.print_location(CURRENT_CASE, __FILE__, __LINE__);                           \
         testing::tests.print_details("FAIL(" #MESSAGE ")");                                        \
-        throw testing::test_state::failed;                                                         \
+        throw testing::impl::test_state::failed;                                                   \
     } while (0)
 
 #define SKIP(MESSAGE)                                                                              \
@@ -238,7 +280,7 @@ const char* proxy<std::tuple<Args...>>::operator=(const F& func) {
         testing::tests.print_skip();                                                               \
         testing::tests.print_location(CURRENT_CASE, __FILE__, __LINE__);                           \
         testing::tests.print_details("SKIP(" #MESSAGE ")");                                        \
-        throw testing::test_state::skipped;                                                        \
+        throw testing::impl::test_state::skipped;                                                  \
     } while (0)
 
 #define REQUIRE_THROWS_AS(EXPRESSION, EXCEPTION)                                                   \
